dd52fef54c2ecb515e2a5a065e50e7110468c074
suminRoh <roosm@naver.com>
2022-04-05 14:36:36 +0900

    tui image editor

---
+++ drawing/static/drawing/js/base.js
@@ -0,0 +1,9 @@
+const ImageEditor = require('tui-image-editor');
+const instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+  cssMaxWidth: 700,
+  cssMaxHeight: 500,
+  selectionStyle: {
+    cornerSize: 20,
+    rotatingPointOffset: 70,
+  },
+});
