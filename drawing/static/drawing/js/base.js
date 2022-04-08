227c5dba98d61faf7664cf8c9f522b2428ec1524
suminRoh <roosm@naver.com>
2022-04-08 23:10:56 +0900

    add function

--- drawing/static/drawing/js/base.js
+++ drawing/static/drawing/js/base.js
@@ -1,9 +1,69 @@
-//const ImageEditor = require("tui-image-editor");
 var editor = new tui.ImageEditor('#tui-image-editor', {
 
-  cssMaxWidth: 7000,
-  cssMaxHeight: 1000,
-  usageStatistics: false,
+    cssMaxWidth: 500,
+    cssMaxHeight: 700,
+    usageStatistics: false,
+    
+  });
 
+
+
+/*
+ setTimeout(() => {
+  
+ }, 1000);
+ */
+ function addIcon_arrow(){  
+  editor.addIcon('arrow');
+}
+
+function addShape_rect(){  
+  console.log("rect");
+  editor.addShape('rect', {
+    stroke: 'black',
+    strokeWidth: 3,
+    width: 50,
+    height: 50,
+    left: 140,
+    top: 70,
+    isRegular: false
 });
+}
+function addShape_circle(){  
+  editor.addShape('circle', {
+    
+    stroke: 'black',
+    strokeWidth: 3,
+    rx: 30,
+    ry: 30,
+    isRegular: false
+});
+}
+function addShape_triangle(){  
+  editor.addShape('triangle', {
+
+    stroke: 'black',
+    strokeWidth: 3,
+    width: 50,
+    height: 50,
+    left: 140,
+    top: 70,
+    isRegular: false
+});
+}
+function addText(){
+  editor.addText('abcd', {
+    styles: {
+        fill: '#000',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    position: {
+        x: 100,
+        y: 100
+    }
+}).then(objectProps => {
+    console.log(objectProps.id);
+});
+}
 
